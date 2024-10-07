9a7f95026262f9f14fc515d490a25e1cb129449c
hard1mpulse <linkup811@gmail.com>
2024-10-07 13:21:21 +0300

    Добвалена распаковка Custom Field Transport Method для будущей передачи в качестве аргумента бэкаперу

--- modules/netbox.py
+++ modules/netbox.py
@@ -1,5 +1,7 @@
 import pynetbox,logging,os
 from dotenv import load_dotenv
+from mako.testing.helpers import result_lines
+
 from modules.configuration_reader import read_yamls_from_dir
 load_dotenv()
 conf_dir = os.getenv('CONFIGURATIONS_DIR')
@@ -24,7 +26,7 @@ def get_devices_list_from_nb_instances(nb_instances: list):
                 for filter in params['filters'] :
                     (filter_name,filter_value),=filter.items()
                     logger.info(f'Selecting devices from filter \"{filter_name}\" on instance {params['url']}...')
-                    filtered_devices = [ device.name for device in nb.dcim.devices.filter(**filter_value)]
+                    filtered_devices = [ get_device_params(device) for device in nb.dcim.devices.filter(**filter_value)]
                     if len(filtered_devices) == 0:
                         logger.warning(f'No devices found on filter \"{filter_name}\" on instance {params['url']}!')
                     else:
@@ -32,7 +34,12 @@ def get_devices_list_from_nb_instances(nb_instances: list):
                     matched_devices.extend(filtered_devices)
             else:
                 logger.info(f'No filters NetBox instance {params['url']} applied! Exporting all devices...')
-                matched_devices = [device.name for device in nb.dcim.devices.all()]
+                matched_devices = [ get_device_params(device) for device in nb.dcim.devices.all()]
             result.update({instance : matched_devices})
             logger.info(f'Found {len(matched_devices)} devices on instance {params['url']}')
+    return result
+
+def get_device_params(nb_device):
+    result={}
+    result.update({"role" : nb_device.role.name, "name" : nb_device.name, "platform" : nb_device.platform.name, "ip_address" : nb_device.primary_ip.address, "transport_method" : nb_device.custom_fields["transport_method"]})
     return result
