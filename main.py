6f6576f7c5b62d4c14976ea7ba6096cfde3c9ebb
hard1mpulse <linkup811@gmail.com>
2024-10-12 01:06:40 +0300

    Added github backup storage method

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os,logging
+import os,logging,time
 from modules.configuration_reader import read_yamls_from_dir, read_yaml_configuration,import_env_params
 from modules.netbox import get_devices_list_from_nb_instances
 from modules.transport_methods.ssh import create_ssh_connection, get_backup_from_device_via_ssh
-from modules.backuper import write_backup
+from modules.backuper import write_backup, push_backups_to_git
 
 env_params=import_env_params()
 backuper_config = read_yaml_configuration(env_params["CONFIGURATIONS_DIR"] + "backuper.yml")
@@ -14,3 +14,6 @@ for instance,devices in get_devices_list_from_nb_instances(nb_instances).items()
     for device in devices:
         if ssh_conn:=create_ssh_connection(device):
             write_backup(get_backup_from_device_via_ssh(ssh_conn),device["name"])
+
+if "github" in backuper_config["storage_method"]:
+    push_backups_to_git()
--- modules/backuper.py
+++ modules/backuper.py
@@ -1,4 +1,4 @@
-import logging,os
+import logging,os,time
 from modules.configuration_reader import read_yaml_configuration,import_env_params
 
 
@@ -11,10 +11,16 @@ backuper_config=read_yaml_configuration(env_params["CONFIGURATIONS_DIR"]+"backup
 def write_backup(backup: str,device: str):
     backuper_config = read_yaml_configuration(env_params["CONFIGURATIONS_DIR"] + "backuper.yml")
     if "local" in backuper_config["storage_method"]:
-        logger.info(f"Local storage method selected for backup! Saving backup of device {device} locally to {env_params["BACKUPS_DIR"]}/local ...")
+        logger.info(f"Local storage method selected for backup! Saving backup of device {device} locally to {env_params["BACKUPS_DIR"]}local ...")
         with open(env_params["BACKUPS_DIR"]+"local/"+device+".backup","w") as file:
             file.write(backup)
     if "github" in backuper_config["storage_method"]:
-        logger.info(f"Github storage method selected for backup! Saving backup of device {device} locally to {env_params["GITHUB_REPO"]}...")
-        with open(env_params["GITHUB_REPO"]+device+".backup","w") as file:
+        logger.info(f"Github storage method selected for backup! Saving backup of device {device} locally to {env_params["GITHUB_DIR"]}...")
+        with open(env_params["GITHUB_DIR"]+device+".backup","w") as file:
             file.write(backup)
+
+def push_backups_to_git():
+    if os.system(f'cd {env_params["GITHUB_DIR"]} && git add *.backup && git commit -m "Added backups from {time.strftime("%d-%m-%Y")}" && git push -u origin master') == 0:
+        logger.info(f"Successfully pushed backups to github from directory {env_params["GITHUB_DIR"]}!")
+    else:
+        logger.info(f"Some error recieved while pushing backups to github from directory {env_params["GITHUB_DIR"]}!")
